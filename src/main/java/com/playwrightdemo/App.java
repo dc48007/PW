8bbeb5176a45ccee5f35feefb79fd662c520a9db
deepakc <deepak.chaudhary@lendingtree.com>
2023-12-26 12:57:55 +0530

    new changes

---
+++ src/main/java/com/playwrightdemo/App.java
@@ -0,0 +1,13 @@
+package com.playwrightdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
